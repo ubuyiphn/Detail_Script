4c13cd906dd895835c5dacb9b610cf9ca8e1609c
ubuy iphn <ubuyiphn@gmail.com>
2021-11-25 17:28:57 +0530

    xpath found pack with try catch

--- Save_Price_Of_API_URL/src/test/java/Ubuy/Save_Price_Of_API_URL/Browser.java
+++ Save_Price_Of_API_URL/src/test/java/Ubuy/Save_Price_Of_API_URL/Browser.java
@@ -1,10 +1,8 @@
 package Ubuy.Save_Price_Of_API_URL;
 
 import java.net.MalformedURLException;
-import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
@@ -17,6 +15,10 @@ public class Browser
     WebDriver driver;
     
     public static JavascriptExecutor js;
+    
+    public int url_count;
+    
+    public Boolean availability;
     
 	public Browser(WebDriver driver)
     {
@@ -41,9 +43,9 @@ public class Browser
 	
 	public void open_new_tabs() throws InterruptedException, MalformedURLException
 	{
-		int url_count = 1;
+		url_count = 1;
 		
-		while(url_count<=10)
+		while(url_count<=1)
 		{
 		
 		    String url = fetch_browser_data.all_urls.get(url_count).toString();
@@ -64,18 +66,39 @@ public class Browser
 		
 	}
 	
+	public Boolean verify_xpath(String x_path)
+	{
+		try
+		{
+			availability = driver.findElement(By.xpath(x_path)).isDisplayed();
+			
+			System.out.println(x_path+""+availability);
+			
+			return availability;
+		}
+		
+		catch(Exception e)
+		{
+			availability = false;
+			
+			System.out.println(x_path+""+availability);
+			
+			return availability;
+		}
+	}
+	
 	public void close_tabs() throws InterruptedException
 	{
 		List<String> tabs = new ArrayList<>(driver.getWindowHandles());
 		
 		int tab_count = 0;
 		
-		while(tab_count<=10)
+		while(tab_count<url_count) 
 		{
 		
 		driver.switchTo().window(tabs.get(tab_count));
 		
-		if(driver.findElement(By.xpath("//h3[@class='h2-heading price']")).isDisplayed())
+		if(verify_xpath("//h3[@class='h2-heading price']") == true)
 		{
 			System.out.println("Price is available.");
 			
@@ -84,7 +107,7 @@ public class Browser
 			tab_count++;
 		}
 		
-		else if(driver.findElement(By.xpath("//div[@class='loader-spin-overlay loading']/div")).isDisplayed())
+		else if(verify_xpath("//div[@class='loader-spin-overlay loading']/div") == true)
 		{
 			System.out.println("Page not refreshed completely.");
 			
@@ -92,28 +115,28 @@ public class Browser
 			
 			Thread.sleep(3000);
 			
-			if(driver.findElement(By.xpath("//h3[@class='h2-heading price']")).isDisplayed())
+			if(verify_xpath("//h3[@class='h2-heading price']") == true)
 			{
 				System.out.println("Price is available.");
 				
 				driver.close();
 			}
 			
-			else if(driver.findElement(By.xpath("//div[@class='product-not-found m-auto']/p")).isDisplayed())
+			else if(verify_xpath("//div[@class='product-not-found m-auto']/p") == true)
 			{
 				System.out.println("Page not found.");
 				
 				driver.close();
 			}
 			
-			else if(driver.findElement(By.xpath("//span[@class='out-of-stock ml-2']")).isDisplayed())
+			else if(verify_xpath("//span[@class='out-of-stock ml-2']") == true)
 			{
 				System.out.println("Out of stock.");
 				
 				driver.close();
 			}
 			
-			else if(driver.findElement(By.xpath("//div[@class='loader-spin-overlay loading']/div")).isDisplayed())
+			else if(verify_xpath("//div[@class='loader-spin-overlay loading']/div") == true)
 			{
 				System.out.println("Page not refreshed completely.");
 				
@@ -124,7 +147,7 @@ public class Browser
 			
 		}
 		
-		else if(driver.findElement(By.xpath("//div[@class='product-not-found m-auto']/p")).isDisplayed())
+		else if(verify_xpath("//div[@class='product-not-found m-auto']/p") == true)
 		{
 			System.out.println("Page not found.");
 			
@@ -133,7 +156,7 @@ public class Browser
 			tab_count++;
 		}
 		
-		else if(driver.findElement(By.xpath("//span[@class='out-of-stock ml-2']")).isDisplayed())
+		else if(verify_xpath("//span[@class='out-of-stock ml-2']") == true)
 		{
 			System.out.println("Out of stock.");
 			
@@ -141,6 +164,7 @@ public class Browser
 			
 			tab_count++;
 		}
+		Thread.sleep(1000);
 		
 	}
 		
